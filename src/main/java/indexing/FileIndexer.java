069fd5600e3c98eb5fb86df7741c45cee7d0bf4c
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:24:30 +0200

    .

--- src/main/java/indexing/FileIndexer.java
+++ src/main/java/indexing/FileIndexer.java
@@ -1,4 +1,27 @@
 package indexing;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FileIndexer {
+    private Map<String, String> fileIndex;
+
+    public FileIndexer() {
+        fileIndex = new HashMap<>();
+    }
+
+    public void indexFiles(String directoryPath) throws IOException {
+        Files.walk(Paths.get(directoryPath))
+                .filter(Files::isRegularFile)
+                .forEach(file -> fileIndex.put(file.getFileName().toString(), file.toString()));
+    }
+
+    public String getFilePath(String fileName) {
+        return fileIndex.get(fileName);
+    }
 }
