a62c4db3eb1f4333bae8407877c827eb74db170b
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:23:52 +0200

    .

--- src/main/java/analysis/RelationAnalyzer.java
+++ src/main/java/analysis/RelationAnalyzer.java
@@ -1,4 +1,13 @@
 package analysis;
 
+import java.util.Map;
+
 public class RelationAnalyzer {
+    public void analyzeRelations(Map<String, String> relations) {
+        for (Map.Entry<String, String> entry : relations.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println("Key: " + key + ", Value: " + value);
+        }
+    }
 }
