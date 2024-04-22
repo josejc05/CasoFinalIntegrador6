ce8cf741fa2609c7d7dda0fabc38f9d1a2ec2744
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:36:55 +0200

    .

--- src/main/java/model/DataList.java
+++ src/main/java/model/DataList.java
@@ -1,4 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataList {
+    private List<String> dataList;
+
+    public DataList() {
+        this.dataList = new ArrayList<>();
+    }
+
+    public void addData(String data) {
+        this.dataList.add(data);
+    }
+
+    public void removeData(String data) {
+        this.dataList.remove(data);
+    }
+
+    public List<String> getDataList() {
+        return this.dataList;
+    }
 }
