dc94b26519f5b6ed1a29a9e856f9fa90294521c3
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:27:40 +0200

    .

--- src/main/java/management/DataAnalyzer.java
+++ src/main/java/management/DataAnalyzer.java
@@ -1,4 +1,11 @@
 package management;
 
+import java.util.List;
+
 public class DataAnalyzer {
+    public void analyzeData(List<String> data) {
+        for (String datum : data) {
+            System.out.println("Data: " + datum);
+        }
+    }
 }
