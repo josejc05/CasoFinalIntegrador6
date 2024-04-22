b99a6cb977efc64d78338ac548bf50dab6c2d25d
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:42:05 +0200

    .

--- src/main/java/sortsearch/DataSearcher.java
+++ src/main/java/sortsearch/DataSearcher.java
@@ -1,4 +1,9 @@
 package sortsearch;
 
+import java.util.List;
+
 public class DataSearcher {
+    public int searchData(List<String> data, String target) {
+        return data.indexOf(target);
+    }
 }
