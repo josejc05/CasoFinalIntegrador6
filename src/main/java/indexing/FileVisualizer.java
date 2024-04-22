e6b220f5c8905c8f8f0deadf1d7c364585422059
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:25:05 +0200

    .

--- src/main/java/indexing/FileVisualizer.java
+++ src/main/java/indexing/FileVisualizer.java
@@ -1,4 +1,17 @@
 package indexing;
 
+import java.util.Map;
+
 public class FileVisualizer {
+    private FileIndexer fileIndexer;
+
+    public FileVisualizer(FileIndexer fileIndexer) {
+        this.fileIndexer = fileIndexer;
+    }
+
+    public void visualizeFiles() {
+        for (Map.Entry<String, String> entry : fileIndexer.getFileIndex().entrySet()) {
+            System.out.println("File: " + entry.getKey() + ", Path: " + entry.getValue());
+        }
+    }
 }
