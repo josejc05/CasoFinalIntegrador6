6bc3cdfb51878ff351933d5a69687548510e3c4e
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:43:10 +0200

    .

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,4 +1,19 @@
 package ui;
 
+import java.util.Scanner;
+
 public class UserInterface {
+    private Scanner scanner;
+
+    public UserInterface() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getInput() {
+        return scanner.nextLine();
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
 }
