f2994c2d3dff573648325a161ea5dc2d1cc714f6
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:28:04 +0200

    .

--- src/main/java/management/DataManager.java
+++ src/main/java/management/DataManager.java
@@ -1,4 +1,24 @@
 package management;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataManager {
+    private List<String> data;
+
+    public DataManager() {
+        this.data = new ArrayList<>();
+    }
+
+    public void addData(String datum) {
+        this.data.add(datum);
+    }
+
+    public void removeData(String datum) {
+        this.data.remove(datum);
+    }
+
+    public List<String> getData() {
+        return this.data;
+    }
 }
