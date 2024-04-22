bd0b044b9bf4a5baecd5b334e6586048bbbc0012
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:37:14 +0200

    .

--- src/main/java/model/Pair.java
+++ src/main/java/model/Pair.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Pair {
+    private String key;
+    private String value;
+
+    public Pair(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getKey() {
+        return this.key;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
