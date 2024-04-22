45f6957cca333c23cf958806fd6517f4b6eaeb53
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:39:15 +0200

    .

--- src/main/java/relation/RelationManager.java
+++ src/main/java/relation/RelationManager.java
@@ -1,4 +1,24 @@
 package relation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RelationManager {
+    private Map<String, String> relations;
+
+    public RelationManager() {
+        this.relations = new HashMap<>();
+    }
+
+    public void addRelation(String key, String value) {
+        this.relations.put(key, value);
+    }
+
+    public void removeRelation(String key) {
+        this.relations.remove(key);
+    }
+
+    public Map<String, String> getRelations() {
+        return this.relations;
+    }
 }
