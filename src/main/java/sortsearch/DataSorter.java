027500b8433caac0fc2245363ae9c0bfc1162dc8
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-22 16:42:34 +0200

    .

--- src/main/java/sortsearch/DataSorter.java
+++ src/main/java/sortsearch/DataSorter.java
@@ -1,4 +1,10 @@
 package sortsearch;
 
+import java.util.Collections;
+import java.util.List;
+
 public class DataSorter {
+    public void sortData(List<String> data) {
+        Collections.sort(data);
+    }
 }
