433c0995ad9d67d0ce4e33e1e2fe2050e62526f2
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-23 10:02:05 +0200

    .

--- src/main/java/retrieval/DataRetriever.java
+++ src/main/java/retrieval/DataRetriever.java
@@ -1,9 +1,10 @@
 package retrieval;
 
+import model.Pair;
 import java.util.List;
 
 public class DataRetriever {
-    public String retrieveData(List<String> data, int index) {
+    public Pair retrieveData(List<Pair> data, int index) {
         if (index >= 0 && index < data.size()) {
             return data.get(index);
         } else {
