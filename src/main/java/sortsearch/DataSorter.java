71986dc4c4e7391e1a2ffc5df32120b918f49ed8
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-23 10:02:25 +0200

    .

--- src/main/java/sortsearch/DataSorter.java
+++ src/main/java/sortsearch/DataSorter.java
@@ -1,10 +1,11 @@
 package sortsearch;
 
+import model.Pair;
 import java.util.Collections;
 import java.util.List;
 
 public class DataSorter {
-    public void sortData(List<String> data) {
-        Collections.sort(data);
+    public void sortData(List<Pair> data) {
+        Collections.sort(data, (Pair p1, Pair p2) -> p1.getKey().compareTo(p2.getKey()));
     }
 }
