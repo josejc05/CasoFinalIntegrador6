5c06b354ae03bbf325f104875bd124da6d07c586
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-23 11:06:21 +0200

    .

--- src/main/java/management/DataAnalyzer.java
+++ src/main/java/management/DataAnalyzer.java
@@ -1,9 +1,23 @@
 package management;
 
 import model.Pair;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DataAnalyzer {
+    public List<Pair> sortSalesByPrice(List<Pair> data) {
+        return data.stream()
+                .sorted(Comparator.comparing(pair -> (Double) pair.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public List<Pair> sortSalesByName(List<Pair> data) {
+        return data.stream()
+                .sorted(Comparator.comparing(pair -> (String) pair.getKey()))
+                .collect(Collectors.toList());
+    }
+
     public void analyzeData(List<Pair> data) {
         for (Pair datum : data) {
             System.out.println("Data: " + datum.getKey() + ", Value: " + datum.getValue());
