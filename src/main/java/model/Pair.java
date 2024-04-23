e988b94a3f7f5034db107b4136d8408b2be1932d
Jose Jurado Cortés <jjuracor@myuax.com>
2024-04-23 11:20:44 +0200

    .

--- src/main/java/model/Pair.java
+++ src/main/java/model/Pair.java
@@ -1,12 +1,16 @@
 package model;
 
+import java.util.Date;
+
 public class Pair {
     private String key;
     private Double value;
+    private Date date;
 
-    public Pair(String key, Double value) {
+    public Pair(String key, Double value, Date date) {
         this.key = key;
         this.value = value;
+        this.date = date;
     }
 
     public String getKey() {
@@ -17,11 +21,19 @@ public class Pair {
         return this.value;
     }
 
+    public Date getDate() {
+        return this.date;
+    }
+
     public void setKey(String key) {
         this.key = key;
     }
 
     public void setValue(Double value) {
         this.value = value;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
     }
 }
